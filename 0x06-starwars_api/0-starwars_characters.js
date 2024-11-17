2e5f1209e1948a46415d16e4e33384158d0d0dd8
Amira Walid <amirawaleed012@gmail.com>
2024-11-17 17:46:45 +0200

    Star Wars Characters

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,19 @@
+const request = require('request');
+const movieID = process.argv[2] || '';
+
+request(`https://swapi-api.alx-tools.com/api/films/${movieID}`, (err, response, body) => {
+  if (err) {
+    console.error('Error:', err);
+  } else if (response.statusCode === 200) {
+    const characters = JSON.parse(body).characters;
+    for (const character of characters) {
+      request(character, (err, response, body) => {
+        if (err) {
+          console.error('Error:', err);
+        } else if (response.statusCode === 200) {
+          console.log(JSON.parse(body).name);
+        }
+      });
+    }
+  }
+});
