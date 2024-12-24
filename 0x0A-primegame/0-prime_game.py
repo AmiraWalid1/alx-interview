211c55a457ffcf2345e83fe00b1094008bf0a2e3
Amira Walid <amirawaleed012@gmail.com>
2024-12-24 18:26:04 +0200

    Prime game

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -2,60 +2,40 @@
 ''' Prime Game'''
 
 
-def isPrime(num):
-    """
-    Check if a number is prime.
-
-    Args:
-    num (int): The number to check.
-
-    Returns:
-    int: 1 if the number is prime, 0 otherwise.
-    """
-    for i in range(2, num):
-        if (num % i == 0):
-            return 0
-    return 1
-
-
-def numOfPrime(num):
-    """
-    Count the number of prime numbers less than or equal to a given number.
-
-    Args:
-    num (int): The upper limit (inclusive) for counting primes.
-
-    Returns:
-    int: The count of prime numbers up to `num`.
-    """
-    cnt = 0
-    for i in range(2, num + 1):
-        cnt += isPrime(i)
-    return cnt
-
-
 def isWinner(x, nums):
     """
-    Determine the winner of a game based on the number of primes
-    in given ranges.
-    Assuming Maria always goes first and both players play optimally.
+    Determine the winner of the prime game for x rounds.
 
     Args:
-    x (int): The number of rounds to play.
-    nums (list): A list of integers, where each integer is the upper limit
+    x (int): Number of rounds.
+    nums (list): List of integers, where each integer is the upper limit
     for a round.
 
     Returns:
-    str: The name of the winner ("Maria" or "Ben").
+    str: The winner of the game ("Maria" or "Ben"). If it's a tie, return None.
     """
-    if (x <= 0):
+    if x <= 0 or not nums:
         return None
+
+    max_num = max(nums)
+
+    is_prime = [True] * (max_num + 1)
+    is_prime[0] = is_prime[1] = False
+
+    for i in range(2, int(max_num**0.5) + 1):
+        if is_prime[i]:
+            for j in range(i * i, max_num + 1, i):
+                is_prime[j] = False
+
+    prime_count = [0] * (max_num + 1)
+    for i in range(1, max_num + 1):
+        prime_count[i] = prime_count[i - 1] + (1 if is_prime[i] else 0)
+
     Maria, Ben = 0, 0
     for i in range(x):
-        # If the number of primes is odd, Maria wins the round; otherwise,
-        # Ben wins.
-        if (numOfPrime(nums[i]) % 2):
+        if (prime_count[nums[i]] % 2):
             Maria += 1
         else:
             Ben += 1
+
     return "Maria" if Maria > Ben else "Ben"
